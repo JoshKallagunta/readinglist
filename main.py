d197038b1de35cb2ac87a947050d14980fbf2896
joshkallagunta <nq6647xv@go.minneapolis.edu>
2019-09-11 22:56:32 -0500

    add print confirmation when book status read, is changed

--- main.py
+++ main.py
@@ -62,11 +62,18 @@ def search_book():
 
 def change_read():
 
-    book_id = ui.get_book_id()
-    book = store.get_book_by_id(book_id)  
-    new_read = ui.get_read_value()     
-    book.read = new_read 
-    book.save()
+#Added a while True loop to print if the user has read or not read the book 
+#based on their user input 
+    while True:
+        book_id = ui.get_book_id()
+        book = store.get_book_by_id(book_id)  
+        new_read = ui.get_read_value()     
+        book.read = new_read 
+        book.save()
+        print('You have read ' + book )
+        if book.read == new_read:
+            print('You have not read ' + book )
+    
     
 
 def quit_program():
